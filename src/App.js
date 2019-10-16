df868ba0a205ff1ef1ba6a7143fe4323a1a04965
momo <niidome55538@gmail.com>
2019-10-16 18:54:43 +0900

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todo from './containers/Todo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todo　/>
+      </div>
+    );
+  }
+}
+
+export default App;
